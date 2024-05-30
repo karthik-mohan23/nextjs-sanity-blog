7104f5cfb63efef3512c5835b96126e4a9b69d65
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-05-30 23:31:50 +0530

    update file structure

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
 import "./globals.css";
-import NavigationBar from "./components/Navbar";
+import NavigationBar from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
